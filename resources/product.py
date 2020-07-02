65bd67a07c220f6667e606976519419a6d3534cc
Murilo Carlos <efgs96@gmail.com>
2020-07-02 01:51:18 -0300

    Product resource updated

--- resources/product.py
+++ resources/product.py
@@ -1,80 +1,55 @@
 from flask_restful import Resource, reqparse
 from models.product import ProductModel
 
-products = [
-        {
-        'productId': 'porto',
-        'productName': 'Vinho do Porto 750ml',
-        'category': 'Vinho',
-        'amount': 16,
-        'description': '',
-        'price': 120.00,
-        'discount': 10
-        },
-        {
-        'productId': 'tanqueray',
-        'productName': 'Gin Tanqueray Dry 750ml',
-        'category': 'Gin',
-        'amount': 28,
-        'description': '',
-        'price': 99.99,
-        'discount': 0
-        },
-        {
-        'productId': 'dom perignon',
-        'productName': 'Champagne Dom Pérignon 750ml',
-        'category': 'Champagne',
-        'amount': 22,
-        'description': '',
-        'price': 1189.99,
-        'discount': 10
-        }
-]
-
 class Products(Resource):
     def get(self):
-        return {'products': products}
+        return {'products': [product.json() for product in ProductModel.query.all()]}
 
 class Product(Resource):
     arguments = reqparse.RequestParser()
-    arguments.add_argument('productName')
-    arguments.add_argument('category')
-    arguments.add_argument('amount')
-    arguments.add_argument('description')
-    arguments.add_argument('price')
-    arguments.add_argument('discount')
-
-    def find_product(productId):
-        for product in products:
-            if product['productId'] == productId:
-                return product
-        return None
+    arguments.add_argument('productName', type=str, required=True, help="The field 'productName' cannot be empty.")
+    arguments.add_argument('category', type=str)
+    arguments.add_argument('amount', type=int, required=True, help="The field 'amount' cannot be empty.")
+    arguments.add_argument('description', type=str)
+    arguments.add_argument('price', type=float, required=True, help="The product 'price' cannot be empty.")
 
     def get(self, productId):
-        product = Product.find_product(productId)
+        product = ProductModel.find_product(productId)
         if product:
-            return product
+            return product.json()
         return {'message': 'Product not found.'}, 404
     
     def post(self, productId):
+        if ProductModel.find_product(productId):
+            return {"message": "Product id '{}' already exists.".format(productId)}, 400
+
         data = Product.arguments.parse_args()
         productObject = ProductModel(productId, **data)
-        newProduct = productObject.json()
-
-        products.append(newProduct)
-        return newProduct, 200
+        try:
+            productObject.save_product()
+        except:
+            return {'message': 'An internal error occurred while trying to save the data.'}, 500
+        return productObject.json()
 
     def put(self, productId):
         data = Product.arguments.parse_args()
-        productObject = ProductModel(productId, **data)
-        newProduct = productObject.json()
-        product = Product.find_product(productId)
-        if product:
-            product.update(newProduct)
-            return product, 200
+        productFound = ProductModel.find_product(productId)
+
+        if productFound:
+            productFound.update_product(**data)
+            try:
+                productFound.save_product()
+            except:
+                return {'message': 'An internal error occurred while trying to update the data.'}, 500
+            return productFound.json(), 200
         return {'messsage': 'Product not found.'}, 404
 
     def delete(self, productId):
-        global products
-        products = [product for product in products if product['productId'] != productId]
-        return {'message': 'Product deleted.'}, 200
+        product = ProductModel.find_product(productId)
+        if product:
+            try:
+                product.delete_product()
+            except:
+                return {'message': 'An internal error occurred while trying to delete the data.'}, 500
+            return {'message': 'Product deleted.'}, 200
+        return {'message': 'Product not found.'}, 404
