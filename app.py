82848fcc34f41ac35834bec025bd7a4df27e8ba7
Murilo Carlos <efgs96@gmail.com>
2020-07-02 01:49:58 -0300

    Run updated

--- app.py
+++ app.py
@@ -3,11 +3,18 @@ from flask_restful import Api
 from resources.product import Products, Product
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
+@app.before_first_request
+def create_db():
+    db.create_all()
 
 api.add_resource(Products, '/api/v1/products')
 api.add_resource(Product, '/api/v1/products/<string:productId>')
 
 if __name__ == '__main__':
+    from sql_alchemy import db
+    db.init_app(app)
     app.run(debug=True)
